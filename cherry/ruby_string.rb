18451d63e43531ab6cdcdf6428367cde00d67eed
sachio <sachio.engineer@gmail.com>
2021-12-10 23:49:39 +0900

    :technologist: Add string出力結果追記

--- cherry/ruby_string.rb
+++ cherry/ruby_string.rb
@@ -1,11 +1,15 @@
 ### 文字列
 name = 'Alice'
-puts "Hello, #{name}!" # 式展開
-puts 'Hello, ' + name + '!' # +演算子で文字列連結
+puts "Hello, #{name}!" #=> Hello, Alice! 式展開
+puts 'Hello, ' + name + '!' #=> Hello, Alice! +演算子で文字列連結
+
 # 機能を打ち消したい場合前にバックスラッシュ
-puts "Hello, \#{name}!"
-puts 'He said, "Don\'t speak."' # シングルクウォートの中にシングルクウォート
-puts "He said, \"Don't speak.\"" # ダブルクウォートの中にダブルクウォート
+puts "Hello, \#{name}!" #=> Hello, #{name}!
+# シングルクウォートの中にシングルクウォート
+puts 'He said, "Don\'t speak."'  #=> He said, "Don't speak."
+# ダブルクウォートの中にダブルクウォート
+puts "He said, \"Don't speak.\""  #=> He said, "Don't speak."
+
 # 文字列中でバックスラッシュの後に記述する文字によっては特別な意味を持たせる事ができる
 # \t #=> タブ \n #=> 改行 など
 puts "シ\n"
@@ -16,11 +20,20 @@ puts "\t\t\t\tレ\n"
 puts "\t\t\t\t\tナ\n"
 puts "\t\t\t\t\t\t〜\n"
 puts "\t\t\t\t\t\t\tイ"
+# シ
+#        ン
+#                ジ
+#                        ラ
+#                                レ
+#                                        ナ
+#                                                〜
+#                                                        イ
+
 # 文字列の比較
-puts 'ruby' == 'ruby'
-puts 'ruby' == 'Ruby'
-puts 'ruby' != 'perl'
-puts 'ruby' != 'ruby'
+puts 'ruby' == 'ruby' #=> true
+puts 'ruby' == 'Ruby' #=> false
+puts 'ruby' != 'perl' #=> true
+puts 'ruby' != 'ruby' #=> false
 
 # %記法で文字列を作る
 # シングルクウォートやダブルクウォートをエスケープする必要がない
@@ -28,17 +41,17 @@ puts 'ruby' != 'ruby'
 # 区切り文字が < { ( [ の場合は終わりの区切り文字が対応するとじかっこ になる
 
 # %q! ! はシングルクウォートと同じ
-puts %q!He said , "Don't speak."!
+puts %q!He said , "Don't speak."! #=> He said , "Don't speak."
 # %Q! ! はダブルクウォートと同じ
 something = 'Hello.'
-puts %Q!He said , "#{something}"!
+puts %Q!He said , "#{something}"! #=> He said , "Hello."
 # %! ! はダブルクウォートと同じ
 something = 'Bye.'
-puts %!He said , "#{something}"!
+puts %!He said , "#{something}"! #=> He said , "Bye."
 # ? を区切り文字として使う
-puts %q?He said , "Don't speak."?
+puts %q?He said , "Don't speak."? #=> He said , "Don't speak."
 # {} を区切り文字として使う
-puts %q{He said , "Don't speak."}
+puts %q{He said , "Don't speak."} #=> He said , "Don't speak."
 
 # ヒアドキュメント(行指向文字列リテラル)
 # 複数行に渡る長い文字列を作成する場合
@@ -49,6 +62,9 @@ a = <<TEXT
 TEXT
 
 puts a
+#=> Aliceさんへ
+#=> これはヒアドキュメントです。
+#=> 複数行に渡る長い文字列を作成するのに便利です。
 
 # 開始ラベルの <<識別子 は1つの式としてみなされるため
 # メソッドの引数として渡したり、<<識別子 に対してメソッドを呼び出せる
@@ -59,32 +75,37 @@ Java
 PHP
 TEXT
 puts a
+#=> Java
+#=> PHP
+#=> Ruby
+
 
 b = <<TEXT.upcase
 Hello,
 Good-bye,
 TEXT
 puts b
+#=> HELLO,
+#=> GOOD-BYE,
 
 # フォーマットを指定して文字列を作成
-#
-# sprintf メソッドか
+# sprintf メソッド、もしくは
 # "フォーマット文字列 % 表示したいオブジェクト" の形式で書く
 
 # 例) 小数点第三位まで数字を表示する文字列を作成する
-puts sprintf('%0.3f', 1.2)
-puts '%0.3f' % 1.2
-puts sprintf('%0.3f + %0.3f', 1.2, 0.48)
-puts '%0.3f + %0.3f' % [1.2, 0.48]
+puts sprintf('%0.3f', 1.2) #=> 1.200
+puts '%0.3f' % 1.2 #=> 1.200
+puts sprintf('%0.3f + %0.3f', 1.2, 0.48) #=> 1.200 + 0.480
+puts '%0.3f + %0.3f' % [1.2, 0.48] #=> 1.200 + 0.480
 
 # そのほかの文字列作成方法
 # 数値を文字列に変換
-puts 123.to_s
+puts 123.to_s #=> 123
 # 配列を連結して文字列にする
-puts [10, 20, 30].join
+puts [10, 20, 30].join #=> 102030
 # *演算子を使って文字列を繰り返す
-puts 'Hi!' * 5
+puts 'Hi!' * 5 #=> Hi!Hi!Hi!Hi!Hi!
 # String.newを使って新しい文字列を作る
-puts String.new('hello')
+puts String.new('hello') #=> hello
 # ダブルクウォートと\uを組み合わせてUnicodeのコードポイントから文字列を作成
-puts "\u3042\u3044\u3046"
+puts "\u3042\u3044\u3046" #=> あいう
