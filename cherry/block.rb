b61e4ccc01bcf6aef8c76a00e209ccc509a329f5
sachio <sachio.engineer@gmail.com>
2021-12-19 14:51:08 +0900

    :technologist: Fin ruby入門4.8.8まで

--- cherry/block.rb
+++ cherry/block.rb
@@ -141,3 +141,145 @@ string = [1, 2, 3, 4].map do |n|
   m.to_s
 end
 p string #=> ["4", "8", "12", "16"]
+
+## 添え字付きの繰り返し処理
+# each_with_indexメソッドはブロック引数の第2引数に添え字を渡す
+fruits = ['apple', 'orange', 'melon']
+fruits.each_with_index { |fruit, i| puts "#{i}: #{fruit}" }
+#=> 0: apple
+#=> 1: melon
+#=> 2: orange
+
+## with_indexメソッドを使った添え字付き繰り返し処理
+# mapメソッドで繰り返し処理をしつつ添字も同時に取得
+fruits.map.with_index { |fruit, i| puts "#{i}: #{fruit}" }
+#=> 0: apple
+#=> 1: melon
+#=> 2: orange
+
+# delete_ifメソッドとの組み合わせ
+fruits.delete_if.with_index { |fruit, i| fruit.include?('a') && i.odd? } #=> ["apple", "melon"]
+
+## 添え字を0以外の数値から開始させる
+# with_indexメソッドに引数を渡すと引数で渡した数値から開始される
+fruits = ['apple', 'orange', 'melon']
+fruits.each.with_index(1) { |fruit, i| puts "#{i}: #{fruit}" }
+#=> 1: apple
+#=> 2: orange
+#=> 3: melon
+p fruits.map.with_index(10) { |fruit, i| "#{i}: #{fruit}" }
+#=> ["10: apple", "11: orange", "12: melon"]
+# each_with_indexには引数を渡せないので注意
+
+## 配列がブロック引数に渡される場合
+# 配列の配列に対して繰り返し処理を実行すると、ブロック引数に配列が渡ってくる
+# 例) 縦の長さと横の長さを配列に格納し、それを複数用意した配列で面積を求める
+dimensions = [[10, 20], [30, 40], [50, 60],]
+areas = []
+# ブロック引数が1つの場合、ブロック引数の値が配列になる
+dimensions.each do |dimension|
+  length = dimension[0]
+  width = dimension[1]
+  areas << length * width
+end
+p areas #=> [200, 1200, 3000]
+
+# 配列の要素分だけブロック引数を用意すると、各要素の値が別々の変数に格納される
+areas = []
+dimensions.each do |length, width|
+  areas << length * width
+end
+p areas #=> [200, 1200, 3000]
+
+# ブロック引数が多すぎる場合、はみ出しているブロック引数はnilになる
+areas = []
+dimensions.each do |length, width, foo, bar|
+  p [length, width, foo, bar]
+end
+#=> [10, 20, nil, nil]
+#=> [30, 40, nil, nil]
+#=> [50, 60, nil, nil]
+
+# 配列の要素が3個あるのに、ブロック引数が2個しかない場合は3つ目は切り捨て
+dimensions = [[10, 20, 100], [30, 40, 200], [50, 60, 300],]
+areas = []
+dimensions.each do |length, width|
+  p [length, width]
+end
+#=> [10, 20]
+#=> [30, 40]
+#=> [50, 60]
+
+# ブロック引数に*をつけると、そのブロック引数に余った分が全て配列として格納される
+areas = []
+dimensions.each do |length, *width|
+  p [length, width]
+end
+#=> [30, [40, 200]]
+#=> [50, [60, 300]]
+#=> [10, [20, 100]]
+
+# each_with_indexのように元からブロック引数を2つ受け取る場合
+# ブロック引数を()で囲むと、配列の要素を別々の引数として受け取ることができる
+dimensions = [[10, 20], [30, 40], [50, 60],]
+areas = []
+dimensions.each_with_index do | (length, width), i |
+  puts "length: #{length}, width: #{width}, i: #{i}"
+end
+#=> length: 10, width: 20, i: 0
+#=> length: 30, width: 40, i: 1
+#=> length: 50, width: 60, i: 2
+
+## ブロックローカル変数
+# ブロック引数を;で区切り、続けて変数を宣言すると、ブロック内でのみ有効な独立したローカル変数を宣言することができる
+numbers = [1, 2, 3, 4]
+sum = 0
+# ブロックの外にあるsumとは別物の変数sumを用意する
+numbers.each do | n; sum |
+  sum = 10
+  sum += n
+  puts sum #=> 11  12  13  14
+end
+# ブロックに中で使っていたsumは別物なので、ブロックの外のsumには変化がない
+p sum #=> 0
+
+## 繰り返し処理以外でも使用されるブロック
+# テキストファイルに文字列を書き込むなど
+# 例) sample.txtを開いて文字列を書き込む
+File.open("./sample.txt", "w") do |file|
+  file.puts("1行目のテキストです。")
+  file.puts("2行目のテキストです。")
+  file.puts("3行目のテキストです。")
+end
+# Rubyの File.openメソッドとブロックを組み合わせると、オープンするだけでなく、「必ずクローズする」という処理までFile.openメソッドがやってくれる
+
+## do...endと{}の結合度の違い
+# 例) 配列のdeleteメソッドにはブロックを渡すと引数で指定した値が見つからない場合の戻り値を指定できる
+a = [1, 2, 3]
+a.delete(100) #=> nil
+a.delete(100) do
+  'NG'
+end #=> "NG"
+# 引数の()を外して記述した場合
+a.delete 100 do
+  'NG'
+end #=> "NG"  これは動く
+# a.delete 100 { 'NG' }
+#=> syntax error, unexpected '{', expecting end-of-input
+# エラーになる 100 { "NG" } と解釈されるため
+a.delete (100) { 'NG' } #=> "NG"  引数を()で囲めば動く
+
+## ブロックの後ろに別のメソッドを続けて書く(メソッドチェーン)
+# 例) 名前の配列をmapメソッドで全部「さん付け」にし、最後に「と」で連結
+names = ['田中', '鈴木', '佐藤']
+san_names = names.map { |name| name + 'さん'}
+san_names.join('と')
+p san_names #=> ["田中さん", "鈴木さん", "佐藤さん"]
+# ブロックの後ろに.をかけば、mapの戻り値に対して直接joinメソッドを呼び出せる
+san_names = names.map { |name| name + 'さん'}.join('と')
+p san_names #=> "田中さんと鈴木さんと佐藤さん"
+# endの後ろに.をつけてメソッドを呼び出すこともできる
+san_names = names.map do |name|
+  "#{name}さん"
+end.join('と')
+p san_names #=> "田中さんと鈴木さんと佐藤さん"
