dbd2165ab5d371c34f3d0e8f231713e79ca187a3
sachio <sachio.engineer@gmail.com>
2022-02-10 23:53:40 +0900

    :technologist: silver対策/予約語 追加

--- silver.rb
+++ silver.rb
@@ -1,73 +1,115 @@
-score = [["kobayashi", 86],["murata", 54],["azuma", 72]]
-score = score.sort {|a,b| -a[1] <=> -b[1]  }
-p score
+# 予約語
+BEGIN	class	ensure	nil	self	when
+END	def	false	not	super	while
+alias	defined?	for	or	then	yield
+and	do	if	redo	true	__LINE__
+begin	else	in	rescue	undef	__FILE__
+break	elsif	module	retry	unless	__ENCODING__
+case	end	next	return	until
 
-cooking = "Rice\nBread\rPasta\n\r"
-p cooking.chomp
+クラス名、変数名には使えない
+ただし$ @ @@といった接頭辞がついたものは予約語としてみなされない
+defのあとやメソッド呼びだしのピリオドなど、メソッド名であることが明白である場所ではメソッド名として使える
 
-a = [2, 3, 6, 8, 12].map{ |d| d / 3 }
-print a
 
-student_1={:name => 'sato', :club => 'tennis'}
-student_2={:name => 'ito', :club => 'football'}
-p student_1.merge(student_2)
+# 同じ動作をするメソッド
+* Array
+slice []
+  指定したインデックスの要素を返す。
+length size
+  要素の数を返します。
+map collect
+  要素の数だけ繰り返しブロックを実行し、ブロックの戻り値を集めた配列を作成
+find detect
+  ブロックを評価して、最初に真となる要素を返す。
+find_all select
+  与えられたブロックを評価した結果の配列を返す。
+delete_if reject!
+  ブロックに要素を渡し、その評価が真になった要素を全て取り除き、自分自身を返す。
 
-score = 50
-battle = score < 70 ? "勝ち" : "負け"
-p battle
 
-true & a = "We"
-false | b = "are"
-2 == 1 && c = "Ruby"
-5 > 3 || d = "Engineer"
-p = a + b + c + d
+* Hash
+merge! update
+  自分自身と引数で指定されたハッシュを統合する。
+length size
+  要素の数を返します。
+has_key? include? key? member?
+  ハッシュが key をキーとして持つ時真を返します。
+has_value? value?
+  ハッシュが value を値として持つ時真を返します。値の一致判定は == で行われます。
+each each_pair
+  ハッシュのキーと値を引数としてブロックを評価します。
+delete_if reject!
+  ブロックを評価した結果が、真になる値を取り除く。
 
-mountain = {"fujisan" => 3776, "asosan" => 1592, "tateyama" => 3015}
-p mountain.length
 
-def teacher(a, *b, c)
-  p  [a, b, c]
-end
+# ！のつかない破壊的メソッド
+* String
+<<
+  文字列の末尾に引数で指定された要素を追加します。
+[]=
+  []内で指定した位置の要素を右辺で置き換えます。
+clear
+  文字列を空文字列に変えます。
+concat
+  文字列の末尾に引数で指定された要素を追加します。
+prepend
+  文字列の先頭に引数で指定された要素を追加します。
+insert
+  第一引数で指定した場所に第二引数で指定した要素を加えます。
+replace
+  引数で指定した要素と置き換えます。
+setbyte
+  第一引数で指定されたインデックスにある要素のバイト数を置き換える。
 
-school = teacher("ito", "kato", "sato", "eto")
-p school
+* Array
+<<
+  要素を末尾に追加します。
+[]=
+  []内で指定した位置の要素を右辺で置き換えます。
+clear
+  array内から全ての要素を削除します。
+concat
+  引数で指定した配列を末尾に連結します。
+delete
+  引数で指定した要素をarray内から削除します。
+delete_at
+  引数で指定したインデックスに該当する要素をarray内から削除します。
+delete_if
+  ブロックで渡した条件に該当する要素をarray内から削除します。
+fill
+  arrayを引数で指定した要素で埋め尽くします。
+insert
+  第一引数で指定したインデックス番号の要素の前に第二引数で指定した要素を挿入します。インデックス番号が負の場合は直後に挿入。
+keep_if
+  要素の数だけ繰り返しブロックを実行し、ブロックの戻り値が真になった要素を残し、偽になった要素を削除します。
+pop
+    arrayの末尾の要素を削除します。
+push
+  要素を末尾に追加します。
+replace
+  引数で指定したarrayと置き換えます
+shift
+  arrayの先頭の要素を削除します。
+unshift
+  arrayの先頭に要素を追加します。
 
-p 2 * 0xBF
-
-puts Integer.superclass
-
-ope = "wakuwaku" == "waku" * 2 || "Rubyは楽しい"
-print ope
-
-color = "red\nblue\ngreen\nblack\nwhite\n"
-s = color.each_line {|i| p i.chomp!}
-print s
-
-p Time.now.to_i
-
-lit = "weloveRubyONrails"[/^[A-Z][^A-Z]+/]
-print lit
-
-p "aa".upto("ae") {|s| print s, ',' }
-
-p sprintf("%#b", 20)
-p sprintf("%#o", 20)
-p sprintf("%#x", 20)
-
-a = ["1", "3", "5", "2", "3"]
-a.delete_at(3)
-p a
-
-h = {:name => 'sato', :club => 'tennis'}.fetch(:name, 'error')
-print h
-
-one = [1, 3, 5]
-two = [2, 4, 6]
-p one.zip(two)
-
-music = "Jazz\nRock\rTechno\n\r!"
-music.chop
-p music
-
-pass = [8, 9, 2, 1] | [2, 7, 4, 1].sort
-p pass
+* Hash
+[]=
+  ハッシュにキーkeyと値valのペアを追加します。keyのキーがすでにあれば、そのキーの値をvalに変更します。
+clear
+  キーと値をすべて削除し、ハッシュを空にします。
+delete
+  ハッシュから引数keyと同じキーを探して、キーと値を削除します。
+delete_if
+  与えられたブロックの戻り値が真になったキーと値を削除します。(ブロック引数key、valにはキーと値が入ります。)
+replace
+  ハッシュの内容を引数で与えられたhashと置き換えます。
+store
+  ハッシュにキーkeyと値valのペアを追加します。keyのキーがすでにあれば、そのキーの値をvalに変更します。(Hash#[]=のエイリアス)
+update
+  レシーバhashの末尾に引数で与えられたhashの内容を加えます。
+keep_if
+  繰り返しブロックを実行し、ブロックの戻り値が真になったキーと値を残し、偽になったものを削除します。
+shift
+  ハッシュの先頭からキーと値を1組削除します。(削除したキーと値を配列にして返します。)
