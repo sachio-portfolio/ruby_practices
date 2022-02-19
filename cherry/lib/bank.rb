a9a8bcd3db3c90d5c620b992bd1cf328eaecdc3f
sachio <sachio.engineer@gmail.com>
2022-02-19 11:25:56 +0900

    :technologist: Fin ruby入門8.5まで

---
+++ cherry/lib/bank.rb
@@ -0,0 +1,7 @@
+require './cherry/lib/deep_freezable'
+
+class Bank
+  extend DeepFreezable
+
+  CURRENCIES = deep_freeze({ 'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee'})
+end
---
+++ cherry/lib/team.rb
@@ -0,0 +1,7 @@
+require './cherry/lib/deep_freezable'
+
+class Team
+  extend DeepFreezable
+
+  COUNTRIES = deep_freeze(['Japan', 'US', 'India'])
+end
